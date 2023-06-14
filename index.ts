6e3b5cde6c78f10ea545c28a6276014727be730a
ducanh2710203 <duc2710203@gmail.com>
2023-06-14 10:35:04 +0700

    cm

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import morgan from "morgan"
+import helmet from "helmet";
 const app = express()
 const port = 4312
 app.use(bodyParser.json())
 app.use(morgan('common'))
+app.use(helmet())
 app.get('/',(req, res) => {
     res.json({
         message : "hê lô , mày yếu không"
